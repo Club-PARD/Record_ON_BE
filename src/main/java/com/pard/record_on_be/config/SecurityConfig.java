7d2381e17a3f2b5d7a8c8181cae76a46720ca3ad
callendev20 <callendev20@gmail.com>
2024-06-25 16:43:00 +0900

    🔒️Add CorsConfig

---
+++ src/main/java/com/pard/record_on_be/config/SecurityConfig.java
@@ -0,0 +1,37 @@
+package com.pard.record_on_be.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final PrincipalOauth2UserService principalOauth2UserService;
+    private final CorsConfig corsConfig;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http)
+            throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable);
+        http.addFilter(corsConfig.corsFilter());
+        http.authorizeHttpRequests(au ->
+                au.anyRequest().permitAll());
+        http.oauth2Login(
+                oauth -> oauth
+                        .loginPage("/loginForm")
+                        .defaultSuccessUrl("/home")
+                        .userInfoEndpoint(
+                                userInfo ->
+                                        userInfo.userService(principalOauth2UserService)
+                        )
+        );
+        return http.build();
+    }
+}
---
+++ src/main/java/com/pard/record_on_be/config/SwaggerConfig.java
@@ -0,0 +1,19 @@
+package com.pard.record_on_be.config;
+
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Record Your__")
+                        .description("서비스 한 줄 설명")
+                        .version("0.9.0"));
+    }
+}
