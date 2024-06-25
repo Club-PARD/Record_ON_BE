86d3a7f92bff31a139af4774e6c31921d31ac055
callendev20 <callendev20@gmail.com>
2024-06-25 16:33:08 +0900

    :tada: Initial commit, Let's Record ON!

---
+++ src/main/java/com/pard/record_on_be/RecordOnBeApplication.java
@@ -0,0 +1,13 @@
+package com.pard.record_on_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecordOnBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecordOnBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pard/record_on_be/RecordOnBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pard.record_on_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecordOnBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
