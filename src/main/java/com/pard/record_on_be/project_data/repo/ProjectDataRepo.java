bf803635ca2bad935dccfd95f75cdb5dcd233681
Giyoul <cla12093@gmail.com>
2024-07-02 19:37:28 +0900

    :white_check_mark: Update Project Data Repository
    Add Repository Anotation

--- src/main/java/com/pard/record_on_be/project_data/repo/ProjectDataRepo.java
+++ src/main/java/com/pard/record_on_be/project_data/repo/ProjectDataRepo.java
@@ -2,6 +2,8 @@ package com.pard.record_on_be.project_data.repo;
 
 import com.pard.record_on_be.project_data.entity.ProjectData;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProjectDataRepo extends JpaRepository<ProjectData, Long> {
 }
