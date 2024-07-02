107ded0717612ca655fae1c207d15e2ccfae97db
Giyoul <cla12093@gmail.com>
2024-07-02 16:21:44 +0900

    :sparkles: Add Project Component
    Add Project Controller
    Add Project DTO
    Add Project Entity
    Add Project Repository
    Add Project Service

---
+++ src/main/java/com/pard/record_on_be/projects/entity/ProjectEntity.java
@@ -0,0 +1,4 @@
+package com.pard.record_on_be.projects.entity;
+
+public class ProjectEntity {
+}
---
+++ src/main/java/com/pard/record_on_be/projects/service/ProjectService.java
@@ -0,0 +1,4 @@
+package com.pard.record_on_be.projects.service;
+
+public class ProjectService {
+}
