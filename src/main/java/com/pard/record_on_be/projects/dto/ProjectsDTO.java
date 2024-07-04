cc007ac57c66ae4bbc6d78045cfefafacd49dda2
Giyoul <cla12093@gmail.com>
2024-07-04 19:20:34 +0900

    :sparkles: Add Projects Main Page View Function

--- src/main/java/com/pard/record_on_be/projects/dto/ProjectsDTO.java
+++ src/main/java/com/pard/record_on_be/projects/dto/ProjectsDTO.java
@@ -2,10 +2,12 @@ package com.pard.record_on_be.projects.dto;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.pard.record_on_be.projects.entity.Projects;
+import com.pard.record_on_be.user.entity.User;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.util.Date;
+import java.util.List;
 import java.util.UUID;
 
 public class ProjectsDTO {
@@ -23,33 +25,40 @@ public class ProjectsDTO {
         private String part;
     }
 
+    @Getter
+    @Setter
+    public static class ReadDefaultPage {
+        private Integer project_id;
+        private String project_name;
+        private String project_image;
+        private Integer is_finished;
+        private List<Integer> competency_tag_id;
+        private List<String> competency_tag_name;
+        private Date start_date;
+        private Date finish_date;
+
+        public ReadDefaultPage(User user, Projects projects, List<Integer> competency_tag_id, List<String> competency_tag_name) {
+            this.project_id = projects.getId();
+            this.project_name = projects.getName();
+            this.project_image = projects.getPicture();
+            this.is_finished = projects.getIs_finished();
+            this.competency_tag_id = competency_tag_id;
+            this.competency_tag_name = competency_tag_name;
+            this.start_date = projects.getStart_date();
+            this.finish_date = projects.getFinish_date();
+        }
+    }
 
     @Getter
     @Setter
     @JsonInclude(JsonInclude.Include.NON_NULL)
-    public static class Read {  // read가 eneity에서 읽어와서 dto로 보내주기
-        private Integer id;
-        private UUID user_id;
-        private String name;
-        private Date start_date;
-        private Date update_date;
-        private Date finish_date;
-        private String description;
-        private String picture;
-        private Integer is_finished;
-        private String part;
+    public static class ReadAll {  // read가 eneity에서 읽어와서 dto로 보내주기
+        private String user_name;
+        private List<ReadDefaultPage> read_default_page;
 
-        public Read(Projects projects) {
-            this.id = projects.getId();
-            this.user_id = projects.getUser_id();
-            this.name = projects.getName();
-            this.start_date = projects.getStart_date();
-            this.update_date = projects.getUpdate_date();
-            this.finish_date = projects.getFinish_date();
-            this.description = projects.getDescription();
-            this.picture = projects.getPicture();
-            this.is_finished = projects.getIs_finished();
-            this.part = projects.getPart();
+        public ReadAll(String user_name, List<ReadDefaultPage> read_default_page) {
+            this.user_name = user_name;
+            this.read_default_page = read_default_page;
         }
     }
 }
--- src/main/java/com/pard/record_on_be/projects/service/ProjectsService.java
+++ src/main/java/com/pard/record_on_be/projects/service/ProjectsService.java
@@ -1,11 +1,56 @@
 package com.pard.record_on_be.projects.service;
 
+import com.pard.record_on_be.projects.dto.ProjectsDTO;
+import com.pard.record_on_be.projects.entity.Projects;
 import com.pard.record_on_be.projects.repo.ProjectsRepo;
+import com.pard.record_on_be.user.entity.User;
+import com.pard.record_on_be.user.repo.UserRepo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
 public class ProjectsService {
     private final ProjectsRepo projectRepo;
+    private final UserRepo userRepo;
+
+    // 사용자의 모든 project를 필요한 내용만 뽑아서 보내주기 위해 dto 만들어서 return 해주기
+    public ProjectsDTO.ReadAll findAll(UUID userId) {
+        List<ProjectsDTO.ReadDefaultPage> readDefaultPageList = new ArrayList<>();
+        User user = userRepo.findById(userId).get();
+        user.getProjects().forEach(project -> {
+            readDefaultPageList.add(
+                    new ProjectsDTO.ReadDefaultPage(
+                            user,
+                            project,
+                            findProjectsTagId(project),
+                            findProjectsTagName(project)));
+        });
+        return new ProjectsDTO.ReadAll(
+                userRepo.findById(userId).get().getName(),
+                readDefaultPageList
+        );
+    }
+
+    //  competency에서 tag id만 뽑아와서 list로 만들어주기
+    public List<Integer> findProjectsTagId(Projects projects) {
+        List<Integer> projectsTagId = new ArrayList<>();
+        projects.getCompetencyTagList().forEach(tag -> {
+            projectsTagId.add(tag.getId());
+        });
+        return projectsTagId;
+    }
+
+    // competency에서 tag 이름만 뽑아와서 list로 만들어주기
+    public List<String> findProjectsTagName(Projects projects) {
+        List<String> projectsTagName = new ArrayList<>();
+        projects.getCompetencyTagList().forEach(tag -> {
+            projectsTagName.add(tag.getName());
+        });
+        return projectsTagName;
+    }
 }
