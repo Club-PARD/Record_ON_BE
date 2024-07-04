3d9e3ed86c1d764011a2f7ab10c56388b1315af6
callendev20 <callendev20@gmail.com>
2024-07-05 02:30:58 +0900

    :bug: Add @EnableJpaAuditing to application so that BaseTimeEntity is automatically saved on user save

--- src/main/java/com/pard/record_on_be/RecordOnBeApplication.java
+++ src/main/java/com/pard/record_on_be/RecordOnBeApplication.java
@@ -2,8 +2,10 @@ package com.pard.record_on_be;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class RecordOnBeApplication {
 
     public static void main(String[] args) {
