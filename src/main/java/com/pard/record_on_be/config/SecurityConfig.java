d72a36f79bcc98d08c999d491cc1478c8d102a2f
callendev20 <callendev20@gmail.com>
2024-07-04 19:08:40 +0900

    ➖ Remove dependency of OAuth2

--- src/main/java/com/pard/record_on_be/config/SecurityConfig.java
+++ src/main/java/com/pard/record_on_be/config/SecurityConfig.java
@@ -1,38 +1,44 @@
 package com.pard.record_on_be.config;
 
-import com.pard.record_on_be.oauth.PrincipalOauth2UserService;
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
-@RequiredArgsConstructor
 public class SecurityConfig {
 
-    private final PrincipalOauth2UserService principalOauth2UserService;
-    private final CorsConfig corsConfig;
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .cors(corsCustomizer -> corsCustomizer.configurationSource(corsConfigurationSource()))
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(au -> au.anyRequest().permitAll());
+        return http.build();
+    }
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http)
-            throws Exception {
-        http.csrf(AbstractHttpConfigurer::disable);
-        http.addFilter(corsConfig.corsFilter());
-        http.authorizeHttpRequests(au ->
-                au.anyRequest().permitAll());
-        http.oauth2Login(
-                oauth -> oauth
-                        .loginPage("/loginForm")
-                        .defaultSuccessUrl("/home")
-                        .userInfoEndpoint(
-                                userInfo ->
-                                        userInfo.userService(principalOauth2UserService)
-                        )
-        );
-        return http.build();
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedOrigin("http://localhost:3000");
+        configuration.addAllowedOrigin("https://api-recordon.shop");
+        configuration.addAllowedHeader("Authorization");
+        configuration.addAllowedHeader("Content-Type");
+        configuration.addAllowedMethod("GET");
+        configuration.addAllowedMethod("POST");
+        configuration.addAllowedMethod("PUT");
+        configuration.addAllowedMethod("DELETE");
+        configuration.setAllowCredentials(true);
+        configuration.setMaxAge(3600L); //preflight 결과를 1시간동안 캐시에 저장
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
