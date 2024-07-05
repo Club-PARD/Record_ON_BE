1a6ed1c3fda82146fd06b5314f7efaeca312d605
Giyoul <cla12093@gmail.com>
2024-07-05 22:17:06 +0900

    :white_check_mark: Update Experiences view page
    Add Controller

--- src/main/java/com/pard/record_on_be/experiences/controller/ExperiencesController.java
+++ src/main/java/com/pard/record_on_be/experiences/controller/ExperiencesController.java
@@ -21,7 +21,9 @@ public class ExperiencesController {
 
     @GetMapping("/experience")
     @Operation(summary = "경험카드 모여있는 전체 페이지 뷰", description = "프로젝트를 처음 클릭했을 때 보여줄 경험들이 모여있는 페이지 입니다.")
+    public ExperiencesDTO.ExperiencesCollectionPageResponse getExperiences(@RequestBody ExperiencesDTO.ExperiencesCollectionPageRequest) {
 
+    }
 
     @GetMapping("/experience/search")
     @Operation(summary = "하나의 프로젝트 대상 경험을 필터링", description = "사용자가 설정한 조건을 갖춘 경험들을 보여줍니다.")
