2c7714c5bf98996f30334ac669dfe21d4efb3dd5
callendev20 <callendev20@gmail.com>
2024-07-05 22:40:35 +0900

    🗑️ Clean up deprecated code - tag, question

--- src/main/java/com/pard/record_on_be/question/repo/QuestionRepo.java
+++
@@ -1,9 +0,0 @@
-package com.pard.record_on_be.question.repo;
-
-import com.pard.record_on_be.question.entity.Question;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface QuestionRepo extends JpaRepository<Question, Integer> {
-}
