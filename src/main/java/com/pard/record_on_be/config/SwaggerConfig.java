0a47b651f5568af843f4ecd968444a3b92fc86b2
callendev20 <callendev20@gmail.com>
2024-07-07 00:58:19 +0900

    🎨 Update SwaggerConfig

--- src/main/java/com/pard/record_on_be/config/SwaggerConfig.java
+++ src/main/java/com/pard/record_on_be/config/SwaggerConfig.java
@@ -12,8 +12,8 @@ public class SwaggerConfig {
     public OpenAPI openAPI() {
         return new OpenAPI()
                 .info(new Info()
-                        .title("Record Your__")
-                        .description("서비스 한 줄 설명")
+                        .title("Record ON")
+                        .description("\"쉽게\",  \"부담감이 없도록\" 오직 경험에 대한 기록에 집중할 수 있도록 도와줍니다. ")
                         .version("0.9.0"));
     }
 }
