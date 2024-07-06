eebc5ca02ce063e43a9cc788fbb3be792a3d3319
Giyoul <cla12093@gmail.com>
2024-07-06 17:26:35 +0900

    :ambulance: Update Mapping Form
    Update Search Projects Mapping type

--- src/main/java/com/pard/record_on_be/experiences/controller/ExperiencesController.java
+++ src/main/java/com/pard/record_on_be/experiences/controller/ExperiencesController.java
@@ -41,13 +41,13 @@ public class ExperiencesController {
         return ResponseEntity.status(response.isSuccess() ? HttpStatus.OK : HttpStatus.FORBIDDEN).body(response);
     }
 
-    @GetMapping()
+    @PostMapping()
     @Operation(summary = "경험카드 모여있는 전체 페이지 뷰", description = "프로젝트를 처음 클릭했을 때 보여줄 경험들이 모여있는 페이지 입니다.")
     public ExperiencesDTO.ExperiencesCollectionPageResponse getExperiences(@RequestBody ExperiencesDTO.ExperiencesCollectionPageRequest experiencesCollectionPageRequest) {
         return experiencesService.findAllExpCollectionPage(experiencesCollectionPageRequest.getProject_id());
     }
 
-    @GetMapping("/search")
+    @PostMapping("/search")
     @Operation(summary = "하나의 프로젝트 대상 경험을 필터링", description = "사용자가 설정한 조건을 갖춘 경험들을 보여줍니다.")
     public List<ExperiencesDTO.ExperienceSearchResponse> getSearchExperiences(@RequestBody ExperiencesDTO.ExperienceSearchRequest experienceSearchRequest) {
         return experiencesService.findExperiencesByFilter(experienceSearchRequest);
