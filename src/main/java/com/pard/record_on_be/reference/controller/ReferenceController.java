747e08a40ea835ea75c8eeb6eae46fbd889175fd
Giyoul <cla12093@gmail.com>
2024-07-06 17:09:31 +0900

    :bug: Fix Search Bug

--- src/main/java/com/pard/record_on_be/reference/controller/ReferenceController.java
+++ src/main/java/com/pard/record_on_be/reference/controller/ReferenceController.java
@@ -4,10 +4,7 @@ import com.pard.record_on_be.reference.dto.ReferenceDTO;
 import com.pard.record_on_be.reference.service.ReferenceService;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 
@@ -17,7 +14,7 @@ import java.io.IOException;
 public class ReferenceController {
     private final ReferenceService referenceService;
 
-    @GetMapping("/reference/get")
+    @PostMapping("/reference/get")
     @Operation(summary = "URL을 메타 데이터로 변환", description = "하나의 링크를 보내주고, 해당 링크에 대한 사이트 이름과, 대표 이미지를 받습니다.")
     public ReferenceDTO.UrlMetadata getMetadata(@RequestBody ReferenceDTO.UrlRequest urlRequest) throws IOException {
         return referenceService.fetchMetadata(urlRequest.getUrl());
