e4aabfe190c12748293abe4d561c7a1efb36a89a
Giyoul <cla12093@gmail.com>
2024-07-07 02:48:18 +0900

    :white_check_mark: Update S3 File conversion controll

--- src/main/java/com/pard/record_on_be/s3/controller/S3TestController.java
+++ src/main/java/com/pard/record_on_be/s3/controller/S3TestController.java
@@ -1,2 +1,30 @@
-package com.pard.record_on_be.s3.controller;public class S3TestController {
+package com.pard.record_on_be.s3.controller;
+
+import com.pard.record_on_be.s3.service.S3Service;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.support.MissingServletRequestPartException;
+
+import java.io.IOException;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api")
+public class S3TestController {
+    private final S3Service s3Service;
+    @PostMapping("/s3")
+    public String uploadProfileImage(@RequestParam("image") MultipartFile multipartFile) throws IOException {
+        if (multipartFile.isEmpty()) {
+            throw new IllegalArgumentException("Uploaded file is empty");
+        }
+        return s3Service.uploadProfile(multipartFile);
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MissingServletRequestPartException.class)
+    public String handleMissingServletRequestPartException(MissingServletRequestPartException ex) {
+        return ex.getMessage(); // or customize error message
+    }
 }
