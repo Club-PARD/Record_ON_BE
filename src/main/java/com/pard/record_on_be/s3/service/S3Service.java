f1625872c48b6c5c3f29387752b542f7258003da
Giyoul <cla12093@gmail.com>
2024-07-07 02:48:48 +0900

    :white_check_mark: Update S3 Service

--- src/main/java/com/pard/record_on_be/s3/service/S3Service.java
+++ src/main/java/com/pard/record_on_be/s3/service/S3Service.java
@@ -1,10 +1,8 @@
 package com.pard.record_on_be.s3.service;
 
 
-import com.amazonaws.services.s3.AmazonS3Client;
-import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.pard.record_on_be.s3.uploader.S3Uploader;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -13,19 +11,12 @@ import java.io.IOException;
 @Service
 @RequiredArgsConstructor
 public class S3Service {
-    private final AmazonS3Client amazonS3Client;
 
-    @Value("${cloud.aws.s3.bucket}")
-    private String bucket;
+    private final S3Uploader s3Uploader;
 
-    public String uploadFile(MultipartFile file) throws IOException {
-        String fileName = file.getOriginalFilename();
-        String fileUrl = "https://" + bucket + ".s3.amazonaws.com/" + fileName;
-        ObjectMetadata metadata = new ObjectMetadata();
-        metadata.setContentType(file.getContentType());
-        metadata.setContentLength(file.getSize());
-        amazonS3Client.putObject(bucket, fileName, file.getInputStream(), metadata);
-        return fileUrl;
+    public String uploadProfile(MultipartFile multipartFile) throws IOException {
+        String url = s3Uploader.upload(multipartFile, "static/profile");
+        System.out.println(url);
+        return url;
     }
-
 }
