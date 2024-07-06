9ae26ec9e759b942ab073571b4dbc4a091cf9bb9
callendev20 <callendev20@gmail.com>
2024-07-06 17:12:46 +0900

    🎨 Change an experience's API address RESTfully

--- src/main/java/com/pard/record_on_be/experiences/controller/ExperiencesController.java
+++ src/main/java/com/pard/record_on_be/experiences/controller/ExperiencesController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/experience")
+@RequestMapping("/api/experiences")
 public class ExperiencesController {
     private final ExperiencesService experiencesService;
 
@@ -23,33 +23,33 @@ public class ExperiencesController {
         this.experiencesService = experiencesService;
     }
 
-    @PostMapping("/create")
+    @PostMapping()
     public ResponseEntity<ResponseDTO> createExperience(@RequestBody ExperiencesDTO.ExperienceInfo experienceInfo) {
         ResponseDTO response = experiencesService.createExperience(experienceInfo);
         return ResponseEntity.status(response.isSuccess() ? HttpStatus.OK : HttpStatus.BAD_REQUEST).body(response);
     }
 
-    @PutMapping("/update/{experienceId}")
-    public ResponseEntity<ResponseDTO> updateExperience(@PathVariable Integer experienceId, @RequestBody ExperiencesDTO.ExperienceInfo experienceInfo) {
-        ResponseDTO response = experiencesService.updateExperience(experienceId, experienceInfo);
-        return ResponseEntity.status(response.isSuccess() ? HttpStatus.OK : HttpStatus.FORBIDDEN).body(response);
-    }
-
-    @DeleteMapping("/delete/{id}")
-    public ResponseEntity<ResponseDTO> deleteExperience(@PathVariable Integer id, @RequestBody UserDTO.UserIdDTO userIdDTO) {
-        ResponseDTO response = experiencesService.deleteExperience(id, userIdDTO.getUser_id());
-        return ResponseEntity.status(response.isSuccess() ? HttpStatus.OK : HttpStatus.FORBIDDEN).body(response);
-    }
-
-    @PostMapping()
-    @Operation(summary = "경험카드 모여있는 전체 페이지 뷰", description = "프로젝트를 처음 클릭했을 때 보여줄 경험들이 모여있는 페이지 입니다.")
-    public ExperiencesDTO.ExperiencesCollectionPageResponse getExperiences(@RequestBody ExperiencesDTO.ExperiencesCollectionPageRequest experiencesCollectionPageRequest) {
-        return experiencesService.findAllExpCollectionPage(experiencesCollectionPageRequest.getProject_id());
-    }
-
-    @PostMapping("/search")
+    @GetMapping("/search")
     @Operation(summary = "하나의 프로젝트 대상 경험을 필터링", description = "사용자가 설정한 조건을 갖춘 경험들을 보여줍니다.")
     public List<ExperiencesDTO.ExperienceSearchResponse> getSearchExperiences(@RequestBody ExperiencesDTO.ExperienceSearchRequest experienceSearchRequest) {
         return experiencesService.findExperiencesByFilter(experienceSearchRequest);
+    }
+
+    @PostMapping("/project")
+    @Operation(summary = "경험카드 모여있는 전체 페이지 뷰", description = "프로젝트를 처음 클릭했을 때 보여줄 경험들이 모여있는 페이지 입니다.")
+    public ExperiencesDTO.ExperiencesCollectionPageResponse getExperiences(@RequestBody ExperiencesDTO.ExperiencesCollectionPageRequest experiencesCollectionPageRequest) {
+        return experiencesService.findAllExpCollectionPage(experiencesCollectionPageRequest.getProject_id(), experiencesCollectionPageRequest.getUser_id());
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ResponseDTO> updateExperience(@PathVariable Integer id, @RequestBody ExperiencesDTO.ExperienceInfo experienceInfo) {
+        ResponseDTO response = experiencesService.updateExperience(id, experienceInfo);
+        return ResponseEntity.status(response.isSuccess() ? HttpStatus.OK : HttpStatus.FORBIDDEN).body(response);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<ResponseDTO> deleteExperience(@PathVariable Integer id, @RequestBody UserDTO.UserIdDTO userIdDTO) {
+        ResponseDTO response = experiencesService.deleteExperience(id, userIdDTO.getUser_id());
+        return ResponseEntity.status(response.isSuccess() ? HttpStatus.OK : HttpStatus.FORBIDDEN).body(response);
     }
 }
