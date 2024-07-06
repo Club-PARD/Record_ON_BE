bf63a07e58b06db9fe2a53b7c72d2c390bc13a32
Giyoul <cla12093@gmail.com>
2024-07-06 23:44:23 +0900

    :white_check_mark: Add S3 Service

---
+++ src/main/java/com/pard/record_on_be/s3/service/S3Service.java
@@ -0,0 +1,31 @@
+package com.pard.record_on_be.s3.service;
+
+
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@Service
+@RequiredArgsConstructor
+public class S3Service {
+    private final AmazonS3Client amazonS3Client;
+
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
+
+    public String uploadFile(MultipartFile file) throws IOException {
+        String fileName = file.getOriginalFilename();
+        String fileUrl = "https://" + bucket + ".s3.amazonaws.com/" + fileName;
+        ObjectMetadata metadata = new ObjectMetadata();
+        metadata.setContentType(file.getContentType());
+        metadata.setContentLength(file.getSize());
+        amazonS3Client.putObject(bucket, fileName, file.getInputStream(), metadata);
+        return fileUrl;
+    }
+
+}
