b4629fdde25ed1ede3865eefdce836ead5739e11
Giyoul <cla12093@gmail.com>
2024-07-08 02:03:35 +0900

    :recycle: Refactor Get Question Dto

--- src/main/java/com/pard/record_on_be/stored_info/dto/StoredTagInfoDTO.java
+++ src/main/java/com/pard/record_on_be/stored_info/dto/StoredTagInfoDTO.java
@@ -8,11 +8,15 @@ public class StoredTagInfoDTO {
     @Getter
     public static class ReadStoredTag {
         private String tag_name;
+        private Integer tag_id;
         private List<String> questions;
+        private List<Integer> question_ids;
 
-        public ReadStoredTag(String tag_name, List<String> questions) {
+        public ReadStoredTag(String tag_name, Integer tag_id, List<String> questions, List<Integer> question_ids) {
             this.tag_name = tag_name;
+            this.tag_id = tag_id;
             this.questions = questions;
+            this.question_ids = question_ids;
         }
     }
 }
