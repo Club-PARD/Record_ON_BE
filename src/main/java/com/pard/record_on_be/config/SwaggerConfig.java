b091a008fe1489c3a923a976098a6c5cad964048
callendev20 <callendev20@gmail.com>
2024-07-07 16:42:24 +0900

    :ambulance: Force Swagger's request to HTTPS

--- src/main/java/com/pard/record_on_be/config/SwaggerConfig.java
+++ src/main/java/com/pard/record_on_be/config/SwaggerConfig.java
@@ -3,14 +3,21 @@ package com.pard.record_on_be.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import io.swagger.v3.oas.models.servers.Server;
 
 @Configuration
 public class SwaggerConfig {
+
+    @Value("${recordon.server.domain}")
+    private String domain;
+
     @Bean
     public OpenAPI openAPI() {
         return new OpenAPI()
+                .addServersItem(new Server().url(domain))
                 .info(new Info()
                         .title("Record ON")
                         .description("\"쉽게\",  \"부담감이 없도록\" 오직 경험에 대한 기록에 집중할 수 있도록 도와줍니다. ")
