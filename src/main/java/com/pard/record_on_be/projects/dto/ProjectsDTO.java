e97539ec89f89e6fe52ed566ce561f259d56e93f
callendev20 <callendev20@gmail.com>
2024-07-08 15:15:54 +0900

    :fire: Remove duplicate DTO

--- src/main/java/com/pard/record_on_be/projects/dto/ProjectsDTO.java
+++ src/main/java/com/pard/record_on_be/projects/dto/ProjectsDTO.java
@@ -24,20 +24,6 @@ public class ProjectsDTO {
 
     @Getter
     @Setter
-    @NoArgsConstructor
-    @AllArgsConstructor
-    public static class Update {
-        private UUID user_id;
-        private String name;
-        private Date start_date;
-        private Date finish_date;
-        private String description;
-        private Integer is_finished;
-        private String part;
-    }
-
-    @Getter
-    @Setter
     public static class ProjectsSearchRequest {
         private UUID user_id;
         private List<String> competency_tag_name;
