29ab01ab26d46af9899cf18ff13eb38cbeaa3519
callendev20 <callendev20@gmail.com>
2024-07-08 22:52:37 +0900

    📝 Add code descriptions

--- src/main/java/com/pard/record_on_be/experiences/service/ExperiencesService.java
+++ src/main/java/com/pard/record_on_be/experiences/service/ExperiencesService.java
@@ -40,15 +40,18 @@ public class ExperiencesService {
         this.projectDataRepo = projectDataRepo;
     }
 
+    // 경험 생성
     @Transactional
     public ResponseDTO createExperience(ExperiencesDTO.ExperienceInfo experienceInfo) {
         try {
+            // 대상 프로젝트의 존재여부 확인
             Projects projects = projectsRepo.findById(experienceInfo.getProjects_id())
                     .orElseThrow(() -> new NoSuchElementException("Project with ID " + experienceInfo.getProjects_id() + " not found"));
 
-            // Check if Question and Tag with id is present
+            // 태그와 질문의 존재여부 확인
             checkQuestionsAndTags(experienceInfo.getQuestion_ids(), experienceInfo.getTag_ids());
 
+            // 경험 생성
             Experiences experience = Experiences.builder()
                     .user_id(experienceInfo.getUser_id())
                     .projects_id(experienceInfo.getProjects_id())
@@ -57,9 +60,12 @@ public class ExperiencesService {
                     .free_content(experienceInfo.getFree_content())
                     .common_question_answer(experienceInfo.getCommon_question_answer())
                     .build();
-            experience.setProjects(projects); // 프로젝트 설정을 빌더 후에 설정
-            experience = experiencesRepo.save(experience); // 저장
 
+            // 프로젝트 설정
+            experience.setProjects(projects);
+            experience = experiencesRepo.save(experience);
+
+            // 답변 기록과 프로젝트 데이터 생성
             createOrUpdateAnswerHistories(experienceInfo, experience);
             createOrUpdateProjectData(experienceInfo, experience);
 
@@ -71,25 +77,31 @@ public class ExperiencesService {
         }
     }
 
+    // 경험 상세보기
     public ResponseDTO getExperienceDetails(Integer experienceId) {
         try {
+            // 대상 경험 존재여부 검사
             Experiences experience = experiencesRepo.findById(experienceId)
                     .orElseThrow(() -> new NoSuchElementException("Experience with ID " + experienceId + " not found"));
 
+            // 답변 기록에서 경험에 해당하는 태그 id 가져오기
             List<Integer> tagIds = experience.getAnswerHistoriesList().stream()
                     .map(AnswerHistories::getTag_id)
                     .toList();
 
+            // 가져온 태그 id 에 해당하는 태그 이름 가져오기
             List<String> tagNames = tagIds.stream()
                     .map(tagId -> storedTagInfoRepo.findById(tagId)
                             .orElseThrow(() -> new NoSuchElementException("Tag with ID " + tagId + " not found"))
                             .getTagName())
                     .toList();
 
+            // 답변 기록에서 경험에 해당하는 질문 아이디 가져오기
             List<Integer> questionIds = experience.getAnswerHistoriesList().stream()
                     .map(AnswerHistories::getQuestion_id)
                     .toList();
 
+            // 가져온 질문 id 에 해당하는 질문 내용 가져오기
             List<String> questionTexts = experience.getAnswerHistoriesList().stream()
                     .map(answerHistories -> storedQuestionInfoRepo
                             .findById(answerHistories.getQuestion_id())
@@ -98,14 +110,17 @@ public class ExperiencesService {
                     )
                     .toList();
 
+            // 답변 기록에서 경험에 해당하는 답변 가져오기
             List<String> questionAnswers = experience.getAnswerHistoriesList().stream()
                     .map(AnswerHistories::getContent)
                     .toList();
 
+            // 프로젝트 데이터에서 경험에 해당하는 참고 링크 가져오기
             List<String> referenceLinks = projectDataRepo.findAllByExperiencesId(experienceId).stream()
                     .map(ProjectData::getReferences_link)
                     .toList();
 
+            // 경험 상세보기 DTO 생성
             ExperiencesDTO.ExperienceDetailsResponse response = new ExperiencesDTO.ExperienceDetailsResponse(
                     experience.getTitle(),
                     experience.getExp_date(),
@@ -130,7 +145,7 @@ public class ExperiencesService {
     // 프론트에서 보내준 필터링 조건에 맞춘 데이터들을 보내주기
     public List<ExperiencesDTO.ExperienceSearchResponse> findExperiencesByFilter(ExperiencesDTO.ExperienceSearchRequest experienceSearchRequest) {
         List<ExperiencesDTO.ExperienceSearchResponse> experienceSearchResponseList;
-        // project의 experience 전체 탐색
+        // project 의 experience 전체 탐색
         experienceSearchResponseList = findExperienceShortByProjectId(experienceSearchRequest.getProject_id());
         // 날짜 선택으로 1차 필터링
         experienceSearchResponseList = findExperienceShortByDate(experienceSearchRequest.getStart_date(), experienceSearchRequest.getFinish_date(), experienceSearchResponseList);
@@ -142,7 +157,7 @@ public class ExperiencesService {
     }
 
     public List<ExperiencesDTO.ExperienceSearchResponse> findExperiencesShortByText(String text, List<ExperiencesDTO.ExperienceSearchResponse> experienceSearchResponseList) {
-        // 1차 필터링: answerHistories에서 text를 포함하는 experiences 찾기
+        // 1차 필터링: answerHistories 에서 text 를 포함하는 experiences 찾기
         List<ExperiencesDTO.ExperienceSearchResponse> expFilteredList = experienceSearchResponseList.stream()
                 .filter(experienceSearchResponse -> {
                     Optional<Experiences> experiencesOptional = experiencesRepo.findById(experienceSearchResponse.getExperience_id());
@@ -194,7 +209,7 @@ public class ExperiencesService {
             expFilteredList.add(experienceSearchResponse);
         });
 
-        // 4차 필터링: free_content에서 text를 포함하는 experiences 찾기, 이미 추가된 experience_id는 제외
+        // 4차 필터링: free_content 에서 text 를 포함하는 experiences 찾기, 이미 추가된 experience_id는 제외
         experienceSearchResponseList.stream().filter(
                 experienceSearchResponse -> {
                     Integer experienceId = experienceSearchResponse.getExperience_id();
@@ -214,7 +229,7 @@ public class ExperiencesService {
         return expFilteredList;
     }
 
-    // 보내준 experience들 중에 tag가 전부 들어간 것들을 전부 찾기
+    // 보내준 experience 들 중에 tag 가 전부 들어간 것들을 전부 찾기
     public List<ExperiencesDTO.ExperienceSearchResponse> findExperiencesShortByTag(List<String> tagNameList, List<ExperiencesDTO.ExperienceSearchResponse> experienceSearchResponseList) {
         if (tagNameList == null || tagNameList.isEmpty()) {
             return experienceSearchResponseList;
@@ -224,7 +239,7 @@ public class ExperiencesService {
         ).collect(Collectors.toList());
     }
 
-
+    // 프로젝트 ID 로 해당하는 경험들 간략하게 가져오기
     public List<ExperiencesDTO.ExperienceSearchResponse> findExperienceShortByProjectId(Integer projectId) {
         List<ExperiencesDTO.ExperienceSearchResponse> readDefaultPageList = new ArrayList<>();
         Projects projects = projectsRepo.findById(projectId).orElseThrow(() -> new RuntimeException("Project not found"));
@@ -246,7 +261,7 @@ public class ExperiencesService {
         return readDefaultPageList;
     }
 
-    // AnswerHistories에서 tag_id만 뽑아와서 List로 만들어주는 메서드
+    // AnswerHistories 에서 tag_id만 뽑아와서 List 로 만들어주는 메서드
     public List<Integer> findExperiencesTagId(List<AnswerHistories> answerHistoriesList) {
         List<Integer> experiencesTagIdList = new ArrayList<>();
         answerHistoriesList.forEach(answerHistories -> experiencesTagIdList.add(answerHistories.getTag_id()));
@@ -263,21 +278,21 @@ public class ExperiencesService {
         return experiencesTagNameList;
     }
 
-    // 날짜 기준으로 sort하는 list 만들어주기
+    // 날짜 기준으로 sort 하는 list 만들어주기
     public List<ExperiencesDTO.ExperienceSearchResponse> findExperienceShortByDate(Date start_date, Date end_date, List<ExperiencesDTO.ExperienceSearchResponse> experienceSearchResponseList) {
         if(start_date == null && end_date == null){ return experienceSearchResponseList; }
         else if(end_date == null){
             return experienceSearchResponseList.stream().filter(
                     experienceSearchResponse -> experienceSearchResponse.getExp_date().after(start_date)
-            ).collect(Collectors.toList());
+            ).toList();
         } else if (start_date == null) {
             return experienceSearchResponseList.stream().filter(
                     experienceSearchResponse -> experienceSearchResponse.getExp_date().before(end_date)
-            ).collect(Collectors.toList());
+            ).toList();
         } else {
             return experienceSearchResponseList.stream().filter(
                     experienceSearchResponse -> experienceSearchResponse.getExp_date().before(end_date) && experienceSearchResponse.getExp_date().after(start_date)
-            ).collect(Collectors.toList());
+            ).toList();
         }
     }
 
@@ -308,24 +323,27 @@ public class ExperiencesService {
         }
     }
 
-
+    // 경험 업데이트
     @Transactional
     public ResponseDTO updateExperience(Integer experienceId, ExperiencesDTO.ExperienceInfo experienceInfo) {
         try {
             Optional<Experiences> optionalExperience = experiencesRepo.findById(experienceId);
+            // 대상 경험의 존재여부 확인
             if (optionalExperience.isPresent()) {
+                // 기존 경험 가져오기
                 Experiences existingExperience = optionalExperience.get();
                 Projects projects = projectsRepo.findById(experienceInfo.getProjects_id())
                         .orElseThrow(() -> new IllegalArgumentException("Project with ID " + experienceInfo.getProjects_id() + " not found"));
 
-                // Check if the user is the owner of the experience
+                // user id 가 기존 경험 생성한 user id 와 같은지 확인
                 if (!existingExperience.getUser_id().equals(experienceInfo.getUser_id())) {
                     return new ResponseDTO(false, "User is not authorized to update this experience");
                 }
 
-                // Check if Question and Tag with id is present
+                // 태그와 질문의 존재여부 확인
                 checkQuestionsAndTags(experienceInfo.getQuestion_ids(), experienceInfo.getTag_ids());
 
+                // 수정된 경험 생성
                 Experiences updatedExperience = Experiences.builder()
                         .id(existingExperience.getId())
                         .user_id(existingExperience.getUser_id())
@@ -337,7 +355,7 @@ public class ExperiencesService {
                         .projects(projects)
                         .build();
 
-                // Update AnswerHistories
+                // 답변 기록 수정
                 Experiences finalUpdatedExperience = updatedExperience;
                 List<AnswerHistories> answerHistoriesList = experienceInfo.getQuestion_ids().stream()
                         .map(questionId -> {
@@ -353,12 +371,12 @@ public class ExperiencesService {
                                     .content(answer)
                                     .experiences(finalUpdatedExperience)
                                     .build();
-                        }).collect(Collectors.toList());
+                        }).toList();
                 updatedExperience.setAnswerHistoriesList(answerHistoriesList);
 
                 updatedExperience = experiencesRepo.save(updatedExperience);
 
-                // Update ProjectData for references
+                // 프로젝트 데이터 수정
                 projectDataRepo.deleteByExperiencesId(updatedExperience.getId());
                 Experiences finalUpdatedExperience1 = updatedExperience;
                 List<ProjectData> projectDataList = experienceInfo.getReference_links().stream()
@@ -368,7 +386,7 @@ public class ExperiencesService {
                                 .references_link(link)
                                 .projects(projects)
                                 .build())
-                        .collect(Collectors.toList());
+                        .toList();
                 projectDataRepo.saveAll(projectDataList);
 
                 return new ResponseDTO(true, "Experience updated successfully", new ExperiencesDTO.Read(updatedExperience));
